2f41a09add3c39ee708f35e057f6be5d487d2a4e
MelarDev <melardev@users.noreply.github.com>
2019-03-08 18:20:13 +0100

    Updates

--- dtos/responses/shared/page_meta.dto.js
+++ dtos/responses/shared/page_meta.dto.js
@@ -10,18 +10,19 @@ module.exports = {
         data.current_page_number = currentPage;
 
         data.prev_page_number = 1;
-        data.total_pages_count = Math.ceil(data.total_items_count / data.requested_page_size);
+        data.number_of_pages = Math.ceil(data.total_items_count / data.requested_page_size);
 
-        if (data.current_page_number < data.total_pages_count) {
+        if (data.current_page_number < data.number_of_pages) {
             data.has_next_page = true;
             data.next_page_number = data.current_page_number + 1;
         } else {
             data.has_next_page = false;
             data.next_page_number = 1;
         }
-        if (data.current_page_number > 1)
+        if (data.current_page_number > 1) {
             data.prev_page_number = data.current_page_number - 1;
-        else {
+            data.has_prev_page = true;
+        } else {
             data.has_prev_page = false;
             data.prev_page_number = 1
         }
